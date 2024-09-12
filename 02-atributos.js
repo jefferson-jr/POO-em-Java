8e66cdc0e333eeac43e0c1fd42dcc314be9b5f88
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-09-12 08:41:14 -0400

    add atributos POO

---
+++ 02-atributos.js
@@ -0,0 +1,9 @@
+class  Pessoa {
+    contructor(nome, idade, altura, peso) { // Atributos 
+        // Inicialização dos atributos
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
