326219d3b025033de68e217fe339855ff54c7c51
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-09-12 10:29:29 -0400

    add exercicio de classe POO

---
+++ 04-exercicio.js
@@ -0,0 +1,22 @@
+class Personagem {
+  constructor(nome, clone, poder, invocacao, velocidade){
+    // atributos
+    this.nome = nome;
+    this.clone = clone;
+    this.poder = poder;
+    this.invocacaao = invocacao;
+    this.velocidade = velocidade;
+  }
+  //Metodos
+  poderEspecial() {
+    console;log(` rasegan ${this.rasegan}!`);
+  }
+ invocacao() {
+    log(`naruto invoca um sapo  ${this.invocacao}!`);
+ }
+     clone() {
+    console;log(`naruto clona ele mesmo ${this.clone}!`);
+
+     }
+  }
+
