849eb348cdea21c9a9c45161dc281745c5340b4c
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-09-19 10:25:01 -0400

    add polimorfismo override

---
+++ 09-Polimorfismo-Override.js
@@ -0,0 +1,16 @@
+// Class base Personagem 
+class Personagem {
+    constructor(nome, vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+    
+    atacar() {
+        console.log(`${this.nome} realizar um ataque basico!`);
+    }
+
+    receberDano(dano) {
+        this.vida -= dano;
+        console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`);
+    }
+}
