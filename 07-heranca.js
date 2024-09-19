ec2bd70993504ec0c24c0c6f1434efac6b6145ad
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-09-19 08:25:26 -0400

    add class objeto

--- 07-heranca.js
+++ 07-heranca.js
@@ -40,3 +40,12 @@ class Professor extends Funcionario {
         super(nome, idade, altura, peso, salario);
     }
 }
+
+
+const aluno = new Estudante("João", 16, 1.70, 70, "Programação de Jogos Digitais");
+const Diretor = new Diretor("Sergio", 45, 1.65, 65, 3000);
+const Professor = new Professor("Wanderson", 40, 1.65, 70, 1000);
+
+console.log(aluno);
+console.log(direitor);
+console.log(professor);
