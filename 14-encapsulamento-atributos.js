4d356769c3133151c8f3f004e9f1fc4d3b446c06
Jefferson <jeffersonjunior.amorim@gmail.com>
2024-10-01 07:49:14 -0400

    add ecncapsulamento atributos

---
+++ 14-encapsulamento-atributos.js
@@ -0,0 +1,15 @@
+class Personagem {
+    #nome;
+    #classe;
+    #nivel;
+    #vida;
+    #mana;
+
+    constructor(nome, classe, nivel, vida, mana) {
+        this.#nome = nome;
+        this.#classe = classe;
+        this.#nivel = nivel;
+        this.#vida = vida;
+        this.#mana = mana;
+    }
+}
